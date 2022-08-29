4e9ab7a0420d4a25c7ffba0a1ceda12b2aedd345
madvirus-ops <edwinayabie1@gmail.com>
2022-08-29 22:14:36 +0100

    social authentiation

--- webber/settings.py
+++ webber/settings.py
@@ -52,6 +52,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -190,3 +194,20 @@ CSRF_TRUSTED_ORIGINS = [
 
 #bbbbggggggggggggggggb
 django_on_heroku.settings(locals())
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+SITE_ID = 2
